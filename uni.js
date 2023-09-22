add860fe9bf315bbe19caa537e73429b01017209
Joe Simpson <josephsimpson94@googlemail.com>
2023-09-22 14:04:19 +0100

    added sprites for pets

--- uni.js
+++ uni.js
@@ -49,3 +49,15 @@ wish.addEventListener("click", () => {
 });
 
 const updateStatsInterval = setInterval(() => updateStats(), 100);
+
+let imageContainer = document.getElementById("imageContainer");
+let images = document.getElementsByTagName("img");
+let currentIndex = 0;
+
+function switchImage() {
+  images[currentIndex].style.display = "none";
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].style.display = "block";
+}
+
+setInterval(switchImage, 1000);
